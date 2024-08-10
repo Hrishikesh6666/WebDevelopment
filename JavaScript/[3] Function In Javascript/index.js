3b5ae8752e47ad2161eb4e067810149d5f03af1b
Hrishikesh6666 <rushikeshshingote6666@gmail.com>
2024-08-10 21:02:48 +0530

    Javascripy

---
+++ JavaScript/[3] Function In Javascript/index.js
@@ -0,0 +1,53 @@
+/*                                                     FUNCTION IN JAVASCRIPT                                            */
+/* Syntax:
+        Function functionName(Parameters){
+        //code to be executed
+        }
+ */
+
+
+function Greet(name){
+    console.log("Hello " + name + " Welcome to the Website");
+}
+Greet("Hrishikesh");
+
+
+                                         /*Function Expression */
+
+/*Syntax:
+          const FunctionName = function(Parameters){
+          //code to be executed
+          
+          }
+ */
+
+const add = function(a,b){
+    return a+b;
+}
+console.log(add(6,4))
+
+/*                                                        ARROW FUNCTION                                                   */
+/*Syntax:
+           const functionName = (Parameters) => {
+            //code to be executed  } ;*/
+
+
+const Multiply = (x,y) => x*y;
+console.log(Multiply(6,6));
+
+//                                     CHAPTER 4 : STRINGS
+
+/*NOTE : Use Quotes (``) to simplifay the code  */
+
+let real_name = "Hrishikesh"
+let Oner = "HS Limited"
+console.log(`${real_name} is the Oner of ${Oner} and he is the best Business man in the INDIA`)
+
+//  OTHER
+let Name = "Hrishikesh"
+Name.length
+Name.toLocaleUpperCase
+Name.toLocaleLowerCase
+Name.slice(0,5)
+Name.replace("Hrishikesh","MAHADAVE")
+
